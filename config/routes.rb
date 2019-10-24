5463fbc66a0212e5d14e22c45d308e6f8ce03bc4
Sim Suri <sim.suri20@gmail.com>
2019-10-23 21:31:56 -0500

    Added tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :class_assignments
+  resources :professors
+  resources :offices
+  resources :classrooms
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
